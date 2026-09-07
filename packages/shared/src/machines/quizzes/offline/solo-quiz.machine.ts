bb21f1300ae16cf494287d15e0089ab3cc44f463
John <jscram1775@gmail.com>
2026-09-07 10:32:38 -0400

    feat(shared): add quiz state machines and offline actions

---
+++ packages/shared/src/machines/quizzes/offline/solo-quiz.machine.ts
@@ -0,0 +1,6 @@
+
+import { createMachine } from "xstate"
+
+export const soloQuizMachine = createMachine({
+    
+})
---
+++ packages/shared/src/types/index.ts
@@ -0,0 +1,20 @@
+import { Quizzes } from "../quizzes";
+
+type QuizType = "solo" | "multiplayer";
+
+
+export interface Quiz {
+    id: string;
+    mode : QuizType
+
+}
+
+
+
+
+export type QuizKey = keyof typeof Quizzes
+
+export type QuizMode = "SOLO"|"VS"
+
+export type DifficultyLevel = "easy" | "medium" | "hard" | "super_hard";
+
